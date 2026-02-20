d27468907a43ba0ffc6ef1305829516472b79812
devtom30 <devtom30@gmail.com>
2026-02-20 17:26:20 +0100

    [feature][drop-import] drop service tests with mock repositories

--- tests/mock/repository/artist.rs
+++ tests/mock/repository/artist.rs
@@ -1,25 +1,26 @@
-use std::sync::RwLock;
+use std::sync::{Arc, RwLock};
 use drop_reverse_proxy::repository::artist::Artist;
 use drop_reverse_proxy::repository::{RepoByName, RepositoryError};
 use std::collections::HashMap;
 
+#[derive(Clone)]
 pub struct ArtistRepoMock {
-    map_by_id: RwLock<HashMap<i32, Artist>>,
-    map_by_name: RwLock<HashMap<String, Artist>>,
+    map_by_id: Arc<RwLock<HashMap<i32, Artist>>>,
+    map_by_name: Arc<RwLock<HashMap<String, Artist>>>,
 }
 
 impl ArtistRepoMock {
     pub fn new() -> Self {
         Self {
-            map_by_id: RwLock::new(HashMap::new()),
-            map_by_name: RwLock::new(HashMap::new())
+            map_by_id: Arc::new(RwLock::new(HashMap::new())),
+            map_by_name: Arc::new(RwLock::new(HashMap::new()))
         }
     }
 
-    pub fn map_by_id(&self) -> &RwLock<HashMap<i32, Artist>> {
+    pub fn map_by_id(&self) -> &Arc<RwLock<HashMap<i32, Artist>>> {
         &self.map_by_id
     }
-    pub fn map_by_name(&self) -> &RwLock<HashMap<String, Artist>> {
+    pub fn map_by_name(&self) -> &Arc<RwLock<HashMap<String, Artist>>> {
         &self.map_by_name
     }
 }
@@ -31,15 +32,9 @@ impl RepoByName<Artist> for ArtistRepoMock {
     }
 
     async fn save_or_update(&self, entity: &Artist) -> Result<i32, RepositoryError> {
-        match self.map_by_id.write().unwrap().insert(entity.id(), entity.clone()) {
-            None => { Err(RepositoryError::EntityNotSaved) }
-            Some(entity) => {  
-                match self.map_by_name().write().unwrap().insert(entity.name().to_string(), entity.clone()) {
-                    None => { Err(RepositoryError::EntityNotSaved) }
-                    Some(_) => { Ok(entity.id()) }
-                }
-            }
-        }
+        self.map_by_id.write().unwrap().insert(entity.id(), entity.clone());
+        self.map_by_name.write().unwrap().insert(entity.name().to_string(), entity.clone());
+        Ok(entity.id())
     }
 
     async fn get_by_name(&self, name: &str) -> Result<Artist, RepositoryError> {
